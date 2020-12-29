351613847aa00a204155e8db8682228029752a85
Antonio Miras <antonio@amiras.net>
2020-12-29 16:14:53 +0100

    Keep both Python and CS examples in test project

---
+++ Test/py/spaCy101.py
@@ -0,0 +1,6 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
